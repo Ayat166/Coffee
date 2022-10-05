6c95ee0c649b8d6749f252229f75410efcc9318b
ayatali1661 <ayatali1661@gmail.com>
2022-10-05 18:05:20 +0200

    initial commit

---
+++ products/models.py
@@ -0,0 +1,19 @@
+from distutils.command.upload import upload
+from pydoc import describe
+from django.db import models
+from datetime import datetime
+# Create your models here.
+
+class Product (models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_active = models.BooleanField(default=True)
+    publish_data = models.DateTimeField(default=datetime.now )
+    
+    def __str__(self):
+        return self.name
+    
+    class Mete:
+        ordering = ['-publish_data']
